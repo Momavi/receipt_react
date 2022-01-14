9ac7684cdae058186a9aecafd0c31fd52ec2ac6e
Momavi <68790545+Momavi@users.noreply.github.com>
2022-01-14 06:06:10 +0300

    finally version

--- src/App.js
+++ src/App.js
@@ -2,36 +2,61 @@ import "./App.css";
 import { adress_pull } from "./data";
 
 function App() {
-  var water_cost = 205; // цена за 1 куб горячей воды в России
-  var electr_cost = 3; // цена за 1 кВт электр. в России
-  var adress_to_out = []; // массив для будущих улиц на вывод
-  var max_water_cost = "";
-  var max_water_cost_id = "";
+  let water_cost = 205; // цена за 1 куб горячей воды в России
+  let electr_cost = 3; // цена за 1 кВт электр. в России
+  let adress_out = []; // массив для будущих улиц на вывод
+  let receipt_out = [];
+  let max_water_cost = "";
+  let max_water_cost_id = "";
   // функция фильтр которую мы вызовем позже чтобы вывести на экран только нужные
-  function filter(item) {
+  function filter_to_adress(item) {
     if (
       (Number(item.house_number) % 2 == false ||
         Number(item.house_number) > 30) &&
-      Number(item.electricity_spent * electr_cost) < 1500 &&
       !(item.street.startsWith("П") || item.street.startsWith("Т"))
     ) {
       return true;
     }
   }
 
+  function filter_to_receipt(item) {
+    for (let i = 0; i < item.length; i++) {
+      if (item[i].electricity_spent * electr_cost > max_water_cost) {
+        max_water_cost = item[i].electricity_spent * electr_cost;
+        max_water_cost_id = item[i].id;
+      }
+    }
+    return true;
+  }
+
   // функция добавления разметки улиц на вывод
-  function add_adress(item, id) {
+  function add_adress(item, id, disable) {
+    let className = "check_wrapper";
+    if (disable) {
+      className += " disable";
+    }
     return (
-      <div className="check_wrapper">
+      <div key={id} className={className}>
         <h3>Квитанция номер: {Number(item.id) + 1}</h3>
-        <div key={id} className="check_border">
-          <h3 className="check_header"></h3>
+        <div className="check_border">
           <p>Улица: {item.street}</p>
           <p>Номер дома: {item.house_number}</p>
           <p>Номер квартиры: {item.apartment_number}</p>
           <br />
         </div>
-        <div>
+      </div>
+    );
+  }
+  function add_receipt(item, id) {
+    let className = "check_wrapper";
+    console.log(id);
+    if (id === Number(max_water_cost_id)) {
+      className += " disable";
+    }
+    return (
+      <div key={id} className={className}>
+        <h3>Квитанция номер: {Number(item.id) + 1}</h3>
+        <div className="check_border">
           <p>
             Счет за воду: {item.water_cubes} * {water_cost} ={" "}
             {item.water_cubes * water_cost}
@@ -48,28 +73,29 @@ function App() {
 
   return (
     <div className="App">
-      <h1 className="App_header">Выписка по квитанциям</h1>
+      <h1 className="receipt_header">Квитанция №1</h1>
       <div className="check">
-        {adress_pull.map((item, id) => {
-          if (filter(item)) {
-            adress_to_out.push(add_adress(item, id));
-          }
-        })}
-        {/* удаление макс знач. по затратам на воду */}
-        {adress_to_out.map((item, id, arr) => {
-          if (
-            item.props.children[2].props.children[0].props.children[6] >
-            max_water_cost
-          ) {
-            max_water_cost =
-              item.props.children[2].props.children[0].props.children[6];
-            max_water_cost_id = id;
-          }
-          console.log(max_water_cost_id);
-          console.log(arr);
-        })}
-        {adress_to_out.slice(0, max_water_cost_id)}
-        {adress_to_out.slice(max_water_cost_id + 1, adress_to_out.length)}
+        {
+          ((filter_to_receipt(adress_pull),
+          adress_pull.map((item, id) => {
+            // удаление макс знач. по затратам на воду
+            receipt_out.push(add_receipt(item, id));
+          })),
+          receipt_out)
+        }
+      </div>
+      <h1 className="App_header">Адрес №2</h1>
+      <div className="check">
+        {
+          (adress_pull.map((item, id) => {
+            if (filter_to_adress(item)) {
+              adress_out.push(add_adress(item, id));
+            } else {
+              adress_out.push(add_adress(item, id, true));
+            }
+          }),
+          adress_out)
+        }
       </div>
     </div>
   );
